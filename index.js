c5e772834f8c0332473556b6c237f9fb3539c0d0
Matthieu Robin <mrobin@ideolys.com>
2020-02-13 16:36:22 +0100

    filter from the require

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ process.env.DEBUG_HIDE_DATE = true;
 process.env.DEBUG_COLORS    = 0;
 process.env.KITTEN_LOGGER   = process.env.KITTEN_LOGGER || process.env.DEBUG || '*';
 
+filter.filter();
+// activate already registered loggers if exist
+for (let namespace in logger.loggers) {
+  logger.loggers[namespace].isEnabled = filter.isEnabled(namespace);
+}
+
 module.exports = {
   createLogger          : logger.logger,
   createPersitentLogger : logger.persistentLogger,
